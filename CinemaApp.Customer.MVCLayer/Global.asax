dfe0358ae893696429ea7990f9ac1c5a0bd716d2
qi le <qile97@gmail.com>
2021-08-08 11:02:21 +0800

    Rename project

---
+++ CinemaApp.Customer.MVCLayer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CinemaApp.Customer.MVCLayer.MvcApplication" Language="C#" %>
---
+++ CinemaApp.Customer.MVCLayer/ViewModel/UserCartDetails.cs
@@ -0,0 +1,19 @@
+﻿using CinemaApp.DomainEntity.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CinemaApp.Customer.MVCLayer.ViewModel
+{
+    public class UserCartDetails
+    {
+        public int Id { get; set; }
+        public int TicketPrice { get; set; }
+        public int MovieHallsId { get; set; }
+        public string MovieTitle { get; set; }
+        public string Seat { get; set; }
+        public string HallNo { get; set; }
+        public IEnumerable<UserCart> _UserCart { get; set; }
+    }
+}
